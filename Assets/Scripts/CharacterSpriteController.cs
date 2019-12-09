2ab5933eb38e10efccae669e8134cd7c926fad27
Alien <david.cardenas@enti.cat>
2019-12-09 18:02:43 +0100

    Adding Sprites and a controller, unfinished

---
+++ Assets/Scripts/CharacterSpriteController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpriteController : MonoBehaviour
+{
+    [SerializeField]private Animator controller;
+    [SerializeField] private bool animationState;
+    [SerializeField] private bool attack2;
+    [SerializeField] private bool attack3;
+    [SerializeField] private int attackCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInChildren<Animator>();
+        attack2 = false;
+        attack3 = false;
+        attackCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //controller.SetTrigger("running");
+        
+        animationState = controller.IsInTransition(0);
+        if (Input.GetKey(KeyCode.D))
+            controller.SetTrigger("running");
+        else
+            controller.ResetTrigger("running");
+
+        if (Input.GetKey(KeyCode.S))
+            controller.SetTrigger("crouch");
+        else
+            controller.ResetTrigger("crouch");
+
+        if (Input.GetKey(KeyCode.W))
+            controller.SetTrigger("jump");
+        else
+            controller.ResetTrigger("jump");
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            controller.SetTrigger("changeidle-1");
+        }
+        else
+            controller.ResetTrigger("changeidle-1");
+
+        if (Input.GetKey(KeyCode.T))
+         {
+                controller.SetTrigger("attack1");
+        }
+        //else
+        //    controller.runtimeAnimatorController = Resources.Load("Animator/idle") as RuntimeAnimatorController;
+    }
+}
