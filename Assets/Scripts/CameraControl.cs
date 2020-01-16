ce446412c034208fc670eff7fa0587e90585f6bf
Roger Sanfeliu <rsanfeliu@enti.cat>
2020-01-16 20:58:39 +0100

    Plataformas, Camara y Testing
    Ya se puede atravesar las plataformas del nivel Castle tanto para subir (botón de Jump) como para bajar (pulsado el botón S). También se puede mover la cámara con las flechas arriba y abajo para ganar un poco de visión extra antes de realizar un salto. La cámara vuelve a su posición inicial tras soltar la tecla. 
    Testting de las plataformas del nivel 1 acabado. Falta el testing de todo el nivel cuando se acabe de hacer la IA de los enemigos.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float speed;
+    public float maxdistance;
+    public GameObject player;
+    Vector3 newPosArriba;
+    Vector3 newPosAbajo;
+    // Update is called once per frame
+    void Update()
+    {
+        //al pulsar el las flechas abajo y arriba la camara del PJ se desplaza hacia dicha dirección.
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+
+        //hacer que cuando la camara llegue a la distancia máxima se quede ahí y no se mueva.
+
+
+        //hacer que cuando se suelta el tecla Up Arrow/DownArrow la camara vuelva a la poscion 0,0,0.
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.SetPositionAndRotation(new Vector3(player.transform.position.x, player.transform.position.y, -300), Quaternion.identity);
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            transform.SetPositionAndRotation(new Vector3 (player.transform.position.x, player.transform.position.y, -300), Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/Scripts/FallFromPlatforms.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFromPlatforms : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime;
+    public float recoverTime;
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+        recoverTime = 0.5f;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Down"))
+        {
+            waitTime = 0f;
+        }
+
+        if (Input.GetButton("Down"))
+        {
+            recoverTime = 0f;
+            if (waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.3f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+
+
+        if (recoverTime >= 0.2f)
+        {
+            effector.rotationalOffset = 0f;
+            recoverTime = 0.2f;
+        }
+        else
+        {
+            recoverTime += Time.deltaTime;
+        }
+
+
+        //Programar que después de pulsar el boton hacia abajo el PJ no haga colision con la plataforma que ha pasado hasta que no la haya sobrepasado completamente.
+
+
+    }
+}
